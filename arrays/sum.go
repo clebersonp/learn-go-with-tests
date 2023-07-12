86da16f02aee48a08cf72a19d0693b81a2a1cab1
Cleberson <pauluci.cleberson@gmail.com>
2023-07-12 11:14:34 -0300

    Adding new func to sumAll numbers of any number of collection

--- arrays/sum.go
+++ arrays/sum.go
@@ -5,5 +5,13 @@ func Sum(numbers []int) int {
 	for _, number := range numbers {
 		sum += number
 	}
-	return sum;
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 // To run cover: $ go test -cover
 
@@ -13,6 +16,17 @@ func TestSum(t *testing.T) {
 		got := Sum(numbers)
 		want := 15
 		assertResult(t, got, want, numbers)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("varargs collection of numbers to sum all", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
 	})
 }
 
