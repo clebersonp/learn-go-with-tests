9a90cd5a11997dced5eed4a7f4d1f361e4aa6e35
Cleberson <pauluci.cleberson@gmail.com>
2023-07-13 23:09:15 -0300

    Added TDD for dependency injection

---
+++ dependency_injection/di.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Greet(writer io.Writer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreetHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie\n")
+
+	// $ go run di.go
+	// open browser at: localhost:5001
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreetHandler)))
+}
