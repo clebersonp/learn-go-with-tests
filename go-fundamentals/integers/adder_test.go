73b508a3f2487bc4b2a0779d63e59f752b0c4515
Cleberson <pauluci.cleberson@gmail.com>
2024-02-17 00:41:42 -0300

    Created test for add func first, principle of TDD.

---
+++ go-fundamentals/integers/adder_test.go
@@ -0,0 +1,11 @@
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	sum := add(2, 2)
+	expected := 4
+	if sum != expected {
+		t.Errorf("expected %q but got %q", expected, sum)
+	}
+}
