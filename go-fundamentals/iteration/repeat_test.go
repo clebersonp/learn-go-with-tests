d03bcc00f7832665f67eb160d85e4c4bd0debf5a
Cleberson <pauluci.cleberson@gmail.com>
2024-02-17 15:38:53 -0300

    Created benchmarks tests

--- go-fundamentals/iteration/repeat_test.go
+++ go-fundamentals/iteration/repeat_test.go
@@ -2,6 +2,8 @@ package iteration
 
 import "testing"
 
+// To run tests and benchmarks = go test -v -bench=.
+
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a")
 	expected := "aaaaa"
@@ -9,4 +11,21 @@ func TestRepeat(t *testing.T) {
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
+}
+
+// BenchmarkRepeat Benchmarks in Go
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
+
+// BenchmarkParallelRepeat Benchmarks in Go
+func BenchmarkParallelRepeat(b *testing.B) {
+	parallel := func(pb *testing.PB) {
+		for pb.Next() {
+			Repeat("a")
+		}
+	}
+	b.RunParallel(parallel)
 }
