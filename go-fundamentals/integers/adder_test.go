50c1f3bb29a30103e5a3813a4cc8a57ba852999a
Cleberson <pauluci.cleberson@gmail.com>
2024-02-17 11:47:58 -0300

    Created function do add two integers and tests

--- go-fundamentals/integers/adder_test.go
+++ go-fundamentals/integers/adder_test.go
@@ -1,11 +1,29 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdder(t *testing.T) {
-	sum := add(2, 2)
+	sum := Add(2, 2)
 	expected := 4
 	if sum != expected {
-		t.Errorf("expected %q but got %q", expected, sum)
+		t.Errorf("expected %d but got %d", expected, sum)
 	}
+}
+
+// ExampleAdd is an example for Godoc
+func ExampleAdd() {
+	x, y := 2, 2
+
+	fmt.Printf("x: %d, y: %d\n", x, y)
+	fmt.Printf("sum: %d\n", Add(x, y))
+
+	// https://go.dev/blog/examples
+	// godoc -http=localhost:6060
+
+	// Output:
+	// x: 2, y: 2
+	// sum: 4
 }
