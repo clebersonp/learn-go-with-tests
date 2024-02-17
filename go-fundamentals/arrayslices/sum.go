59187cf126ae2735c066df60532dee3022844231
Cleberson <pauluci.cleberson@gmail.com>
2024-02-17 17:18:46 -0300

    Added doc and chaged traditional for loop to for range

--- go-fundamentals/arrayslices/sum.go
+++ go-fundamentals/arrayslices/sum.go
@@ -1,8 +1,12 @@
 package arrayslices
 
+// Sum - range lets you iterate over an array.
+// On each iteration, range returns two values - the index and the value.
+// We are choosing to ignore the index value by using _ blank identifier
+// Array size is encoded in its type. If you try to pass an [4]int into a function that expects [5]int, it won't compile.
 func Sum(numbers [5]int) (result int) {
-	for i := 0; i < 5; i++ {
-		result += numbers[i]
+	for _, num := range numbers {
+		result += num
 	}
 	return
 }
