185a59d0941a780e944495662cf1d507e6e558d7
Cleberson <pauluci.cleberson@gmail.com>
2024-02-18 23:21:43 -0300

    Refactor first tests of func Area to call it as a circle and rectangle structs methods

--- go-fundamentals/structsmethodsinterfaces/v3/shapes_test.go
+++ go-fundamentals/structsmethodsinterfaces/v3/shapes_test.go
@@ -21,7 +21,7 @@ func TestArea(t *testing.T) {
 			Width:  12.0,
 			Height: 6.0,
 		}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 72.0
 
 		if got != want {
@@ -31,7 +31,7 @@ func TestArea(t *testing.T) {
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := Area(circle)
+		got := circle.Area()
 		want := 314.1592653589793
 
 		if got != want {
