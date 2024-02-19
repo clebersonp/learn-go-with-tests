47b1a53827510062e63e7f718c08427ebbe881c8
Cleberson <pauluci.cleberson@gmail.com>
2024-02-19 10:17:38 -0300

    Created v6 to continue lecture

---
+++ go-fundamentals/pointererrors/v6/wallet.go
@@ -0,0 +1,36 @@
+package v6
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Bitcoin is a custom type from existing int type.
+// It's more descriptive
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) WithDraw(amount Bitcoin) error {
+	if amount > w.balance {
+		// errors.New creates a new error with a custom message
+		return errors.New("cannot withdraw, insufficient funds")
+	}
+
+	w.balance -= amount
+	return nil
+}
---
+++ go-fundamentals/pointererrors/v6/wallet_test.go
@@ -0,0 +1,50 @@
+package v6
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	assertError := func(t testing.TB, got error, want string) {
+		t.Helper()
+		// nil is synonymous with null from other programming languages.
+		// and it can be nil because it's an error interface.
+		// interface in Go can be nil
+		if got == nil {
+			// fatal will stop the test if it's called.
+			t.Fatal("wanted an error but didnt' get one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got %q want %q", got.Error(), want)
+		}
+	}
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.WithDraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(10)
+		wallet := Wallet{balance: startingBalance}
+		// in Go to indicate an error it is idiomatic to function return an err to check and act on.
+		err := wallet.WithDraw(Bitcoin(15))
+		assertError(t, err, "cannot withdraw, insufficient funds")
+		assertBalance(t, wallet, startingBalance)
+	})
+}
