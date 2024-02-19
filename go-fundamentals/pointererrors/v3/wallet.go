9ca881c84080f3f8aa02779a0e4e0ec80c0b4d15
Cleberson <pauluci.cleberson@gmail.com>
2024-02-19 09:31:02 -0300

    Added v3 to pointer errors lecture

---
+++ go-fundamentals/pointererrors/v3/wallet.go
@@ -0,0 +1,34 @@
+package v3
+
+import "fmt"
+
+// Bitcoin is a custom type from existing int type.
+// It's more descriptive
+type Bitcoin int
+
+// String method override type Stringer interface
+// It lets us define how type is printed when used with the %s format string in prints.
+// It likes toString() in java
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	// with lower case we are restricting its visibility, so balance property is private outside the package
+	// to access its value we use Balance method to do that
+	balance Bitcoin
+}
+
+// Deposit - putting the notation * in front of Wallet type we are saying to Go that
+// this is special type - "a pointer (address memory) to a wallet
+// Therefore, when we change this wallet value, we are changing the wallet value in memory
+// to access the value of wallet we need to dereference the pointer like: (*w).balance, but the language permits us to
+// write w.balance, without explicit dereference. Go will make that for us automatically
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Printf("address of balance in Deposit method is %p \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
---
+++ go-fundamentals/pointererrors/v3/wallet_test.go
@@ -0,0 +1,31 @@
+package v3
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	// create an empty wallet
+	wallet := Wallet{}
+
+	// put amount into wallet
+	wallet.Deposit(10)
+
+	// return the current wallet amount
+	got := wallet.Balance()
+
+	// In Go, when you call a function or a method the arguments are copied
+	// When calling func (w Wallet) Deposit(amount int) the w is a copy this wallet
+	// to access this wallet address in memory we use symbol '&' in front of a wallet.
+	fmt.Printf("address of balance in test is %p \n", &wallet.balance)
+
+	// To create and instantiate a custom int type like: CustomType(value)
+	want := Bitcoin(11)
+
+	// assertion
+	if got != want {
+		// changed %d (underling int type) to %q or %s to user String override method on Bitcoin
+		t.Errorf("got %q want %q", got, want)
+	}
+}
