6964e05cf76a4b4af3df5e0e7f9c50c707479856
Cleberson <pauluci.cleberson@gmail.com>
2024-02-19 14:37:17 -0300

    Added doc to explain the reason of key can only be of comparable type

--- go-fundamentals/maps/v1/dictionary_test.go
+++ go-fundamentals/maps/v1/dictionary_test.go
@@ -4,6 +4,8 @@ import "testing"
 
 func TestSearch(t *testing.T) {
 	// map syntax -> map[key-type]value-type{initialization-key:value}
+	// key type is special. it can only be a comparable type
+	// the value type, on the other hand, can be any type
 	dictionary := map[string]string{"test": "this is just a test"}
 	key := "test"
 	got := Search(dictionary, key)
