6787ca5bfde7e69323d3451502dd1ee96c3c0887
Cleberson <pauluci.cleberson@gmail.com>
2024-02-19 16:46:50 -0300

    Added more map documentation for Add function

--- go-fundamentals/maps/v4/dictionary.go
+++ go-fundamentals/maps/v4/dictionary.go
@@ -21,6 +21,13 @@ func (d Dictionary) Search(key string) (value string, err error) {
 // So when you pass a map to a function/method, you are indeed copying it, but just the pointer part
 // Map can be nil value. A nil map behaves like an empty map when READING.
 // But attempts to write a nil map will cause a runtime panic
+// to void panic with nil map, you should never initialize an empty map variable like this: var m map[string]string
+// Instead, you can initialize an empty map like this: var dictionary = map[string]string{}
+// OR
+// var dictionary = make(map[string]string)
+// make is a built-in function in Go to create things
+// Both approaches create an empty hash map and point dictionary at it. Which ensures that you will never get a runtime
+// panic
 func (d Dictionary) Add(key, value string) {
 	d[key] = value
 }
