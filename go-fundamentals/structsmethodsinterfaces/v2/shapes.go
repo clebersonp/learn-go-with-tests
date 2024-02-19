9e24ef9c2a398cc8cea6fc232b2acca148034ea4
Cleberson <pauluci.cleberson@gmail.com>
2024-02-18 22:55:43 -0300

    Created v2 where we will use struct and pass it as parameter for function. TDD, refactor test before implement

---
+++ go-fundamentals/structsmethodsinterfaces/v2/shapes.go
@@ -0,0 +1,14 @@
+package v2
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(width, height float64) float64 {
+	return 2 * (width + height)
+}
+
+func Area(width, height float64) float64 {
+	return width * height
+}
