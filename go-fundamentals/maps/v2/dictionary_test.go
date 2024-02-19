90386039a87b8d139d6ecb4943a7e4383fce8e61
Cleberson <pauluci.cleberson@gmail.com>
2024-02-19 14:49:38 -0300

    First test, refactored test and change map type to Dictionary custom type

--- go-fundamentals/maps/v2/dictionary_test.go
+++ go-fundamentals/maps/v2/dictionary_test.go
@@ -3,12 +3,10 @@ package v2
 import "testing"
 
 func TestSearch(t *testing.T) {
-	// map syntax -> map[key-type]value-type{initialization-key:value}
-	// key type is special. it can only be a comparable type
-	// the value type, on the other hand, can be any type
+
 	key := "tests"
-	dictionary := map[string]string{key: "this is just a test"}
-	got := Search(dictionary, key)
+	dictionary := Dictionary{key: "this is just a test"}
+	got := dictionary.Search(key)
 	want := "this is just a test"
 	assertStrings(t, got, want)
 }
