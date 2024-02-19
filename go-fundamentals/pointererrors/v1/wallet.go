d7e63a6b316da46a48836cb91359db3e065d407d
Cleberson <pauluci.cleberson@gmail.com>
2024-02-19 08:38:51 -0300

    Added comment about visibility to a symbol in Go(variables, func, types)

--- go-fundamentals/pointererrors/v1/wallet.go
+++ go-fundamentals/pointererrors/v1/wallet.go
@@ -1,11 +1,15 @@
 package v1
 
-type Wallet struct{}
+type Wallet struct {
+	// with lower case we are restricting its visibility, so balance property is private outside the package
+	// to access its value we use Balance method to do that
+	balance int
+}
 
 func (w Wallet) Deposit(amount int) {
-
+	w.balance += amount
 }
 
 func (w Wallet) Balance() int {
-	return 0
+	return w.balance
 }
