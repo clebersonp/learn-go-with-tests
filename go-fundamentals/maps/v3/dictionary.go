123868c2fb327cda1b3720f1f942c438daf055eb
Cleberson <pauluci.cleberson@gmail.com>
2024-02-19 15:50:51 -0300

    Writed enough code to make testSearch for maps pass

--- go-fundamentals/maps/v3/dictionary.go
+++ go-fundamentals/maps/v3/dictionary.go
@@ -1,7 +1,16 @@
 package v3
 
+import "errors"
+
 type Dictionary map[string]string
 
 func (d Dictionary) Search(key string) (value string, err error) {
+	// map can return 2 values. the first is the value and the second is a boolean which indicates if the key
+	// was found successfully
+	value, ok := d[key]
+	if !ok {
+		return value, errors.New("could not find the key you were looking for")
+	}
+
 	return d[key], nil
 }
