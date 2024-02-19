cece71627501bee40aeb9656af6c6807f9fbb3b0
Cleberson <pauluci.cleberson@gmail.com>
2024-02-19 14:51:02 -0300

    Created custom type Dictionary that it's an underlying map[string]string

--- go-fundamentals/maps/v2/dictionary.go
+++ go-fundamentals/maps/v2/dictionary.go
@@ -1,5 +1,7 @@
 package v2
 
-func Search(dictionary map[string]string, key string) (value string) {
-	return dictionary[key]
+type Dictionary map[string]string
+
+func (d Dictionary) Search(key string) (value string) {
+	return d[key]
 }
