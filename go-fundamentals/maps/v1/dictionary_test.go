7dcb97378d1219f616cce0cdd8bf4fd7f178c010
Cleberson <pauluci.cleberson@gmail.com>
2024-02-19 14:45:57 -0300

    Implemented Search func and refactored test

--- go-fundamentals/maps/v1/dictionary_test.go
+++ go-fundamentals/maps/v1/dictionary_test.go
@@ -6,12 +6,16 @@ func TestSearch(t *testing.T) {
 	// map syntax -> map[key-type]value-type{initialization-key:value}
 	// key type is special. it can only be a comparable type
 	// the value type, on the other hand, can be any type
-	dictionary := map[string]string{"test": "this is just a test"}
-	key := "test"
+	key := "tests"
+	dictionary := map[string]string{key: "this is just a test"}
 	got := Search(dictionary, key)
 	want := "this is just a test"
+	assertStrings(t, got, want)
+}
 
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q given key %q", got, want, key)
+		t.Errorf("got %q want %q", got, want)
 	}
 }
