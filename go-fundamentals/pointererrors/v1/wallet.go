2014f122c4ce2d784bd64c5b58d4bc6c56999c23
Cleberson <pauluci.cleberson@gmail.com>
2024-02-19 09:10:45 -0300

    Fixed tests, changing wallet receiver to a pointer and add comments about that

--- go-fundamentals/pointererrors/v1/wallet.go
+++ go-fundamentals/pointererrors/v1/wallet.go
@@ -8,11 +8,16 @@ type Wallet struct {
 	balance int
 }
 
-func (w Wallet) Deposit(amount int) {
+// Deposit - putting the notation * in front of Wallet type we are saying to Go that
+// this is special type - "a pointer (address memory) to a wallet
+// Therefore, when we change this wallet value, we are changing the wallet value in memory
+// to access the value of wallet we need to dereference the pointer like: (*w).balance, but the language permits us to
+// write w.balance, without explicit dereference. Go will make that for us automatically
+func (w *Wallet) Deposit(amount int) {
 	fmt.Printf("address of balance in Deposit method is %p \n", &w.balance)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
 	return w.balance
 }
