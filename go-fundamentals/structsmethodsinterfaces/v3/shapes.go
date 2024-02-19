981f77da770e3d7a9137b29353d8577966ac979a
Cleberson <pauluci.cleberson@gmail.com>
2024-02-18 23:33:44 -0300

    Added new comments to circle Area method

--- go-fundamentals/structsmethodsinterfaces/v3/shapes.go
+++ go-fundamentals/structsmethodsinterfaces/v3/shapes.go
@@ -15,13 +15,18 @@ func Perimeter(rec Rectangle) float64 {
 	return 2 * (rec.Width + rec.Height)
 }
 
-func (rec Rectangle) Area() float64 {
-	return rec.Width * rec.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
 // Area - func (receiver type) name(...parameters type) return type {}
 // This is a method of circle instance type
-func (circle Circle) Area() float64 {
+// func (receiverName ReceiverType) MethodName(args)
+// When your method is called on a variable of that type,
+// you get your reference to its data via the receiverName variable.
+// In many other programming languages this is done implicitly, and you access the receiver via 'this'.
+// It is a convention in Go to have the receiver variable be the first letter of the type
+func (c Circle) Area() float64 {
 	// Area = π × r^2
-	return math.Pi * math.Pow(circle.Radius, 2)
+	return math.Pi * math.Pow(c.Radius, 2)
 }
