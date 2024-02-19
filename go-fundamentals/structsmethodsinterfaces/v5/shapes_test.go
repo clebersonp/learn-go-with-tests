0801b69cbdb86743dec53014d25079bc1d8a5d86
Cleberson <pauluci.cleberson@gmail.com>
2024-02-19 00:31:05 -0300

    Refactored TestArea v5 tests, add a new field called name to print the name of perform test and used t.Run method

--- go-fundamentals/structsmethodsinterfaces/v5/shapes_test.go
+++ go-fundamentals/structsmethodsinterfaces/v5/shapes_test.go
@@ -22,18 +22,21 @@ func TestArea(t *testing.T) {
 	// table driven tests.
 	// Are useful when you want to build a list of test cases that can be tested in the same manner
 	areaTableTest := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Width: 12.0, Height: 6.0}, want: 72.0},
-		{shape: Circle{Radius: 10.0}, want: 314.1592653589793},
-		{shape: Triangle{12.0, 6.0}, want: 36.0},
+		{name: "rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "circle", shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793},
+		{name: "triangle", shape: Triangle{12.0, 6.0}, hasArea: 36.0},
 	}
 
 	for _, table := range areaTableTest {
-		got := table.shape.Area()
-		if got != table.want {
-			t.Errorf("got %.2f want %.2f", got, table.want)
-		}
+		t.Run(table.name, func(t *testing.T) {
+			got := table.shape.Area()
+			if got != table.hasArea {
+				t.Errorf("%#v got %.2f want %.2f", table.shape, got, table.hasArea)
+			}
+		})
 	}
 }
