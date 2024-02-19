b209f85770488ede0af35f60330000fa1d896bfc
Cleberson <pauluci.cleberson@gmail.com>
2024-02-18 22:50:20 -0300

    Renamed Area func parameters

--- go-fundamentals/structsmethodsinterfaces/v1/shapes.go
+++ go-fundamentals/structsmethodsinterfaces/v1/shapes.go
@@ -4,6 +4,6 @@ func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
 }
 
-func Area(x, y float64) float64 {
-	return x * y
+func Area(width, height float64) float64 {
+	return width * height
 }
