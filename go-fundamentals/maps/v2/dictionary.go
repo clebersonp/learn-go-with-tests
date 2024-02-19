cbf353ce66f358b96f73270a7fe235c7a5208576
Cleberson <pauluci.cleberson@gmail.com>
2024-02-19 14:47:05 -0300

    Created v2 of maps to continue lecture

---
+++ go-fundamentals/maps/v2/dictionary.go
@@ -0,0 +1,5 @@
+package v2
+
+func Search(dictionary map[string]string, key string) (value string) {
+	return dictionary[key]
+}
---
+++ go-fundamentals/maps/v2/dictionary_test.go
@@ -0,0 +1,21 @@
+package v2
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	// map syntax -> map[key-type]value-type{initialization-key:value}
+	// key type is special. it can only be a comparable type
+	// the value type, on the other hand, can be any type
+	key := "tests"
+	dictionary := map[string]string{key: "this is just a test"}
+	got := Search(dictionary, key)
+	want := "this is just a test"
+	assertStrings(t, got, want)
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
