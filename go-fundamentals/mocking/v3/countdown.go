dda4b9f9317bbae9f34fc1a5bedfa20d603697ca
Cleberson <pauluci.cleberson@gmail.com>
2024-02-20 12:12:41 -0300

    Created new version 3 to continues lecture about mocking

---
+++ go-fundamentals/mocking/v3/countdown.go
@@ -0,0 +1,20 @@
+package v3
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(out, finalWord)
+}
