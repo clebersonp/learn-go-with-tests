c217b9f908866be7fadda5a9efc99a54d1bbb10f
Cleberson <pauluci.cleberson@gmail.com>
2024-02-21 23:49:48 -0300

    Created an example of convergence with go routines and channels

---
+++ go-fundamentals/concurrency/chains/convergence/v1/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"math/rand/v2"
+	"time"
+)
+
+func main() {
+	// iterate n times about the new received channel and get elements
+	convergence := converge(hardWork("upload-images"), hardWork("convert-pdf"))
+
+	for i := 0; i < 30; i++ {
+		fmt.Println(<-convergence) // receiving the converged two channels values
+	}
+}
+
+// hardWork simulate a hard work
+func hardWork(name string) chan string {
+	ch := make(chan string)
+	go func(s string, c chan string) {
+		for i := 0; ; i++ {
+			c <- fmt.Sprintf("%v \t- %d", s, i)
+			// take time to do the work
+			time.Sleep(time.Duration(rand.Int64N(1e3)) * time.Millisecond)
+		}
+	}(name, ch)
+	return ch
+}
+
+// convergence converge two channels into one
+func converge(a, b chan string) chan string {
+	// this is a closure function because it's passing other variables out of it scope
+	convergence := make(chan string)
+	go func() {
+		for {
+			select {
+			case convergence <- <-a: // receive from channel a and send to convergence channel
+			case convergence <- <-b: // receive from channel b and send to convergence channel
+			}
+		}
+	}()
+	return convergence
+}
