8aa37566963715e92fe4676590ebf4dadaa906cc
Cleberson <pauluci.cleberson@gmail.com>
2024-02-23 14:25:23 -0300

    Refactored racer test, created two server to test the requests

--- go-fundamentals/concurrency/select/v2/racer_test.go
+++ go-fundamentals/concurrency/select/v2/racer_test.go
@@ -1,10 +1,32 @@
 package racer
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "https://www.facebook.com"
-	fastURL := "https://quii.dev"
+
+	// start a test server to simulate a response with delay
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(15 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer slowServer.Close()
+
+	// start a test server to simulate the faster response without delay
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+	fmt.Printf("Try Slow:\t%q\n", slowURL)
+	fmt.Printf("Try Fast:\t%q\n", fastURL)
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
