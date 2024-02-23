efd81e8719e51db2fe28581ba88d26f7e22fd000
Cleberson <pauluci.cleberson@gmail.com>
2024-02-23 14:38:49 -0300

    Refactored to avoid repetition DRY

--- go-fundamentals/concurrency/select/v2/racer_test.go
+++ go-fundamentals/concurrency/select/v2/racer_test.go
@@ -11,16 +11,12 @@ import (
 func TestRacer(t *testing.T) {
 
 	// start a test server to simulate a response with delay
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(15 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
-	defer slowServer.Close()
-
+	slowServer := makeDelayedServer(15 * time.Millisecond)
 	// start a test server to simulate the faster response without delay
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	// defer to close servers at the end of performing this block of code (TestRacer function scope)
+	defer slowServer.Close()
 	defer fastServer.Close()
 
 	slowURL := slowServer.URL
@@ -34,4 +30,12 @@ func TestRacer(t *testing.T) {
 	if want != got {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+	return slowServer
 }
