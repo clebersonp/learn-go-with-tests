d4d94ec45dadb6e98efc6d26142e161133fc8a5b
Cleberson <pauluci.cleberson@gmail.com>
2024-02-22 21:20:10 -0300

    Added a package directory for test

---
+++ go-fundamentals/handlererrors/package/main.go
@@ -0,0 +1,9 @@
+// Package something is a test package
+package something
+
+import "fmt"
+
+// Do is a function that prints some text to standard output
+func Do() {
+	fmt.Println("Doing something")
+}
