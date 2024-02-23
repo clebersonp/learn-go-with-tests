861208882563fb6e964b422ab7ee6a2ca03ccbec
Cleberson <pauluci.cleberson@gmail.com>
2024-02-22 21:25:36 -0300

    Deleted file

--- go-fundamentals/handlererrors/package/main.go
+++
@@ -1,9 +0,0 @@
-// Package something is a test package
-package something
-
-import "fmt"
-
-// Do is a function that prints some text to standard output
-func Do() {
-	fmt.Println("Doing something")
-}
