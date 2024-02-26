8581067a396622ec607e94994f0ab8e247b6c4b4
Cleberson <pauluci.cleberson@gmail.com>
2024-02-26 14:00:34 -0300

    Created v3 for reflections test

---
+++ go-fundamentals/reflection/v3/reflection_test.go
@@ -0,0 +1,44 @@
+package v3
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+
+	// table test
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			Name: "struct with one string field",
+			Input: struct {
+				Name string
+			}{"Chris"},
+			ExpectedCalls: []string{"Chris"},
+		},
+		{
+			Name: "struct with two string fields",
+			Input: struct {
+				Name string
+				City string
+			}{Name: "Chris", City: "London"},
+			ExpectedCalls: []string{"Chris", "London"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %q, want %q", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
