41e359582144bcfdf51f43fd80e7bc3c3c527ced
Cleberson <pauluci.cleberson@gmail.com>
2024-02-26 20:03:50 -0300

    Created v7 version for reflection to continue testing

---
+++ go-fundamentals/reflection/v7/reflection_test.go
@@ -0,0 +1,86 @@
+package v7
+
+import (
+	"reflect"
+	"testing"
+)
+
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
+func TestWalk(t *testing.T) {
+
+	// table test
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			Name: "struct with one string field",
+			Input: struct {
+				Name string
+			}{"Chris"},
+			ExpectedCalls: []string{"Chris"},
+		},
+		{
+			Name: "struct with two string fields",
+			Input: struct {
+				Name string
+				City string
+			}{Name: "Chris", City: "London"},
+			ExpectedCalls: []string{"Chris", "London"},
+		},
+		{
+			Name: "struct with non string field",
+			Input: struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			ExpectedCalls: []string{"Chris"},
+		},
+		{
+			Name: "nested fields",
+			Input: Person{
+				Name:    "Chris",
+				Profile: Profile{Age: 33, City: "London"},
+			},
+			ExpectedCalls: []string{"Chris", "London"},
+		},
+		{
+			Name: "pointers to things",
+			Input: &Person{
+				Name:    "Chris",
+				Profile: Profile{Age: 33, City: "London"},
+			},
+			ExpectedCalls: []string{"Chris", "London"},
+		},
+		{
+			Name: "slices",
+			Input: []Profile{
+				{Age: 33, City: "London"},
+				{Age: 34, City: "California"},
+			},
+			ExpectedCalls: []string{"London", "California"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %q, want %q", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
