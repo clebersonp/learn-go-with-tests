fd6074cac73bbc281ef8e212c77a00e93a7f0ccb
Cleberson <pauluci.cleberson@gmail.com>
2024-02-27 22:33:20 -0300

    Created first version of context test

---
+++ go-fundamentals/context/v1/context_test.go
@@ -0,0 +1,41 @@
+package v1
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, q *http.Request) {
+		fmt.Fprint(w, store.Fetch())
+	}
+}
+
+type Store interface {
+	Fetch() string
+}
+
+type StubStore struct {
+	response string
+}
+
+func (s *StubStore) Fetch() string {
+	return s.response
+}
+
+func TestServer(t *testing.T) {
+	data := "hello, world"
+	srv := Server(&StubStore{response: data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	srv.ServeHTTP(response, request)
+
+	resp := response.Body.String()
+	if resp != data {
+		t.Errorf("got %q, want %q", resp, data)
+	}
+}
