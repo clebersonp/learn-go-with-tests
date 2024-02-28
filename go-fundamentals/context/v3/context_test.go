53c23007133d0c2aee88c5f631cc7375f9889992
Cleberson <pauluci.cleberson@gmail.com>
2024-02-27 23:32:13 -0300

    Created new version v3 to continues context lecture

---
+++ go-fundamentals/context/v3/context_test.go
@@ -0,0 +1,101 @@
+package v3
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+
+		data := make(chan string, 1)
+
+		go func() {
+			data <- store.Fetch()
+		}()
+
+		select {
+		case d := <-data:
+			fmt.Fprint(w, d)
+		case <-ctx.Done():
+			store.Cancel()
+		}
+	}
+}
+
+type Store interface {
+	Fetch() string
+	Cancel()
+}
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+	t         *testing.T
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
+func (s *SpyStore) assertWasCancelled() {
+	s.t.Helper()
+	if !s.cancelled {
+		s.t.Error("store was not told to cancel")
+	}
+}
+
+func (s *SpyStore) assertWasNotCancelled() {
+	s.t.Helper()
+	if s.cancelled {
+		s.t.Error("store was told to cancel")
+	}
+}
+
+func TestServer(t *testing.T) {
+	t.Run("returns data from store", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data, t: t}
+		srv := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
+
+		srv.ServeHTTP(response, request)
+
+		resp := response.Body.String()
+		if resp != data {
+			t.Errorf("got %q, want %q", resp, data)
+		}
+
+		store.assertWasNotCancelled()
+	})
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data, t: t}
+		srv := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		// One of the main points of context is that it is a consistent way of offering cancellation.
+		ctx, cancelFunc := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancelFunc)
+		request = request.WithContext(ctx)
+
+		response := httptest.NewRecorder()
+
+		srv.ServeHTTP(response, request)
+
+		store.assertWasCancelled()
+	})
+}
