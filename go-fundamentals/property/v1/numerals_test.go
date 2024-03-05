360991aaccaee4f84e1d6cbdf21476cd2b340b6a
Cleberson <pauluci.cleberson@gmail.com>
2024-03-05 20:51:36 -0300

    Test first, created the numerals_test.go file to test convertion of arabic numbers to roman

---
+++ go-fundamentals/property/v1/numerals_test.go
@@ -0,0 +1,13 @@
+package v1
+
+import "testing"
+
+func TestConvertToRoman(t *testing.T) {
+	got := ConvertToRoman(1)
+
+	want := "I"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
