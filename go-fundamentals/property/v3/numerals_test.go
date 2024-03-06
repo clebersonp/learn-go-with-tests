d2c30386d75b4177d8a23b4468bdeec430b0fa5e
Cleberson <pauluci.cleberson@gmail.com>
2024-03-05 21:16:28 -0300

    Writed test first for number 3 conversion for roman

--- go-fundamentals/property/v3/numerals_test.go
+++ go-fundamentals/property/v3/numerals_test.go
@@ -9,6 +9,7 @@ func TestConvertToRoman(t *testing.T) {
 	}{
 		{Description: "1 gets converted to I", Arabic: 1, Want: "I"},
 		{Description: "2 gets converted to II", Arabic: 2, Want: "II"},
+		{Description: "3 gets converted to III", Arabic: 3, Want: "III"},
 	}
 	for _, test := range cases {
 		t.Run(test.Description, func(t *testing.T) {
