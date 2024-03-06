7b9b04c796d1fdd30969834eb1ef8f62d4d30133
Cleberson <pauluci.cleberson@gmail.com>
2024-03-05 22:46:10 -0300

    Writed test first, write test function to test conversion of roman to arabic number

--- go-fundamentals/property/v7/numerals_test.go
+++ go-fundamentals/property/v7/numerals_test.go
@@ -6,78 +6,89 @@ import (
 )
 
 var cases = []struct {
-	Want   string
+	Roman  string
 	Arabic int
 }{
-	{Arabic: 1, Want: "I"},
-	{Arabic: 2, Want: "II"},
-	{Arabic: 3, Want: "III"},
-	{Arabic: 4, Want: "IV"},
-	{Arabic: 5, Want: "V"},
-	{Arabic: 6, Want: "VI"},
-	{Arabic: 7, Want: "VII"},
-	{Arabic: 8, Want: "VIII"},
-	{Arabic: 9, Want: "IX"},
-	{Arabic: 10, Want: "X"},
-	{Arabic: 11, Want: "XI"},
-	{Arabic: 12, Want: "XII"},
-	{Arabic: 13, Want: "XIII"},
-	{Arabic: 14, Want: "XIV"},
-	{Arabic: 15, Want: "XV"},
-	{Arabic: 16, Want: "XVI"},
-	{Arabic: 17, Want: "XVII"},
-	{Arabic: 18, Want: "XVIII"},
-	{Arabic: 19, Want: "XIX"},
-	{Arabic: 20, Want: "XX"},
-	{Arabic: 21, Want: "XXI"},
-	{Arabic: 22, Want: "XXII"},
-	{Arabic: 23, Want: "XXIII"},
-	{Arabic: 24, Want: "XXIV"},
-	{Arabic: 25, Want: "XXV"},
-	{Arabic: 26, Want: "XXVI"},
-	{Arabic: 27, Want: "XXVII"},
-	{Arabic: 28, Want: "XXVIII"},
-	{Arabic: 29, Want: "XXIX"},
-	{Arabic: 30, Want: "XXX"},
-	{Arabic: 31, Want: "XXXI"},
-	{Arabic: 32, Want: "XXXII"},
-	{Arabic: 33, Want: "XXXIII"},
-	{Arabic: 34, Want: "XXXIV"},
-	{Arabic: 35, Want: "XXXV"},
-	{Arabic: 36, Want: "XXXVI"},
-	{Arabic: 37, Want: "XXXVII"},
-	{Arabic: 38, Want: "XXXVIII"},
-	{Arabic: 39, Want: "XXXIX"},
-	{Arabic: 40, Want: "XL"},
-	{Arabic: 41, Want: "XLI"},
-	{Arabic: 42, Want: "XLII"},
-	{Arabic: 43, Want: "XLIII"},
-	{Arabic: 44, Want: "XLIV"},
-	{Arabic: 45, Want: "XLV"},
-	{Arabic: 46, Want: "XLVI"},
-	{Arabic: 47, Want: "XLVII"},
-	{Arabic: 48, Want: "XLVIII"},
-	{Arabic: 49, Want: "XLIX"},
-	{Arabic: 50, Want: "L"},
-	{Arabic: 100, Want: "C"},
-	{Arabic: 400, Want: "CD"},
-	{Arabic: 500, Want: "D"},
-	{Arabic: 900, Want: "CM"},
-	{Arabic: 1000, Want: "M"},
-	{Arabic: 1984, Want: "MCMLXXXIV"},
-	{Arabic: 3999, Want: "MMMCMXCIX"},
-	{Arabic: 2014, Want: "MMXIV"},
-	{Arabic: 1006, Want: "MVI"},
-	{Arabic: 798, Want: "DCCXCVIII"},
+	{Arabic: 1, Roman: "I"},
+	{Arabic: 2, Roman: "II"},
+	{Arabic: 3, Roman: "III"},
+	{Arabic: 4, Roman: "IV"},
+	{Arabic: 5, Roman: "V"},
+	{Arabic: 6, Roman: "VI"},
+	{Arabic: 7, Roman: "VII"},
+	{Arabic: 8, Roman: "VIII"},
+	{Arabic: 9, Roman: "IX"},
+	{Arabic: 10, Roman: "X"},
+	{Arabic: 11, Roman: "XI"},
+	{Arabic: 12, Roman: "XII"},
+	{Arabic: 13, Roman: "XIII"},
+	{Arabic: 14, Roman: "XIV"},
+	{Arabic: 15, Roman: "XV"},
+	{Arabic: 16, Roman: "XVI"},
+	{Arabic: 17, Roman: "XVII"},
+	{Arabic: 18, Roman: "XVIII"},
+	{Arabic: 19, Roman: "XIX"},
+	{Arabic: 20, Roman: "XX"},
+	{Arabic: 21, Roman: "XXI"},
+	{Arabic: 22, Roman: "XXII"},
+	{Arabic: 23, Roman: "XXIII"},
+	{Arabic: 24, Roman: "XXIV"},
+	{Arabic: 25, Roman: "XXV"},
+	{Arabic: 26, Roman: "XXVI"},
+	{Arabic: 27, Roman: "XXVII"},
+	{Arabic: 28, Roman: "XXVIII"},
+	{Arabic: 29, Roman: "XXIX"},
+	{Arabic: 30, Roman: "XXX"},
+	{Arabic: 31, Roman: "XXXI"},
+	{Arabic: 32, Roman: "XXXII"},
+	{Arabic: 33, Roman: "XXXIII"},
+	{Arabic: 34, Roman: "XXXIV"},
+	{Arabic: 35, Roman: "XXXV"},
+	{Arabic: 36, Roman: "XXXVI"},
+	{Arabic: 37, Roman: "XXXVII"},
+	{Arabic: 38, Roman: "XXXVIII"},
+	{Arabic: 39, Roman: "XXXIX"},
+	{Arabic: 40, Roman: "XL"},
+	{Arabic: 41, Roman: "XLI"},
+	{Arabic: 42, Roman: "XLII"},
+	{Arabic: 43, Roman: "XLIII"},
+	{Arabic: 44, Roman: "XLIV"},
+	{Arabic: 45, Roman: "XLV"},
+	{Arabic: 46, Roman: "XLVI"},
+	{Arabic: 47, Roman: "XLVII"},
+	{Arabic: 48, Roman: "XLVIII"},
+	{Arabic: 49, Roman: "XLIX"},
+	{Arabic: 50, Roman: "L"},
+	{Arabic: 100, Roman: "C"},
+	{Arabic: 400, Roman: "CD"},
+	{Arabic: 500, Roman: "D"},
+	{Arabic: 900, Roman: "CM"},
+	{Arabic: 1000, Roman: "M"},
+	{Arabic: 1984, Roman: "MCMLXXXIV"},
+	{Arabic: 3999, Roman: "MMMCMXCIX"},
+	{Arabic: 2014, Roman: "MMXIV"},
+	{Arabic: 1006, Roman: "MVI"},
+	{Arabic: 798, Roman: "DCCXCVIII"},
 }
 
 func TestConvertToRoman(t *testing.T) {
 
 	for _, test := range cases {
-		t.Run(fmt.Sprintf("ConvertToRoman(%d) = %q,", test.Arabic, test.Want), func(t *testing.T) {
+		t.Run(fmt.Sprintf("ConvertToRoman(%d) = %q,", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
-			if got != test.Want {
-				t.Errorf("ConvertToRoman(%d) = %q, want %q", test.Arabic, got, test.Want)
+			if got != test.Roman {
+				t.Errorf("ConvertToRoman(%d) = %q, want %q", test.Arabic, got, test.Roman)
+			}
+		})
+	}
+}
+
+func TestConvertToArabic(t *testing.T) {
+	for _, test := range cases[:1] {
+		t.Run(fmt.Sprintf("ConvertToArabic(%q) = %d,", test.Roman, test.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("ConvertToArabic(%q) = %d, want %d", test.Roman, got, test.Arabic)
 			}
 		})
 	}
