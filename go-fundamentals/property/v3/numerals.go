b03d5fb8e7d5567ac8f99d22fa50806c81c94c91
Cleberson <pauluci.cleberson@gmail.com>
2024-03-05 21:17:38 -0300

    Writed enough code to pass v3 test of arabic conversion to roman numbers

--- go-fundamentals/property/v3/numerals.go
+++ go-fundamentals/property/v3/numerals.go
@@ -2,6 +2,9 @@ package v3
 
 // ConvertToRoman converts an integer to a roman numeral
 func ConvertToRoman(arabic int) string {
+	if arabic == 3 {
+		return "III"
+	}
 	if arabic == 2 {
 		return "II"
 	}
