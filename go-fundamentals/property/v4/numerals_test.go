76d06b62af2bba3eaca944693c4643a39cf30eef
Cleberson <pauluci.cleberson@gmail.com>
2024-03-05 21:25:02 -0300

    Writed test first to test conversion of number 4 to IV

--- go-fundamentals/property/v4/numerals_test.go
+++ go-fundamentals/property/v4/numerals_test.go
@@ -10,6 +10,7 @@ func TestConvertToRoman(t *testing.T) {
 		{Description: "1 gets converted to I", Arabic: 1, Want: "I"},
 		{Description: "2 gets converted to II", Arabic: 2, Want: "II"},
 		{Description: "3 gets converted to III", Arabic: 3, Want: "III"},
+		{Description: "4 gets converted to IV", Arabic: 4, Want: "IV"},
 	}
 	for _, test := range cases {
 		t.Run(test.Description, func(t *testing.T) {
