21fe7f684a01a0167a561e6d7943e26fa967360a
Cleberson <pauluci.cleberson@gmail.com>
2024-03-05 21:14:03 -0300

    Refatored v2 test of arabic convertion to roman number and make the test more idiomatic

--- go-fundamentals/property/v2/numerals_test.go
+++ go-fundamentals/property/v2/numerals_test.go
@@ -3,18 +3,18 @@ package v2
 import "testing"
 
 func TestConvertToRoman(t *testing.T) {
-	tests := []struct {
-		name, want string
-		arabic     int
+	cases := []struct {
+		Description, Want string
+		Arabic            int
 	}{
-		{name: "1 gets converted to I", arabic: 1, want: "I"},
-		{name: "2 gets converted to II", arabic: 2, want: "II"},
+		{Description: "1 gets converted to I", Arabic: 1, Want: "I"},
+		{Description: "2 gets converted to II", Arabic: 2, Want: "II"},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := ConvertToRoman(tt.arabic)
-			if got != tt.want {
-				t.Errorf("ConvertToRoman(%d) = %q, want %q", tt.arabic, got, tt.want)
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("ConvertToRoman(%d) = %q, want %q", test.Arabic, got, test.Want)
 			}
 		})
 	}
