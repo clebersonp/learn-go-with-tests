81c038f89b0b0d5dc853ec66d7e9603cd061631f
Cleberson <pauluci.cleberson@gmail.com>
2024-03-05 21:26:11 -0300

    Writed enough code to pass for conversion of number 4 to roman

--- go-fundamentals/property/v4/numerals.go
+++ go-fundamentals/property/v4/numerals.go
@@ -4,6 +4,11 @@ import "strings"
 
 // ConvertToRoman converts an integer to a roman numeral
 func ConvertToRoman(arabic int) string {
+
+	if arabic == 4 {
+		return "IV"
+	}
+
 	var result strings.Builder
 
 	for i := 0; i < arabic; i++ {
