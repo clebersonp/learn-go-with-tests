3187df6a2f55476e8dd87d0ed06f4544f5509237
Cleberson <pauluci.cleberson@gmail.com>
2024-03-05 21:07:50 -0300

    Refactored TestConvertToRoman test and created table-driven test approach

--- go-fundamentals/property/v1/numerals_test.go
+++ go-fundamentals/property/v1/numerals_test.go
@@ -3,11 +3,18 @@ package v1
 import "testing"
 
 func TestConvertToRoman(t *testing.T) {
-	got := ConvertToRoman(1)
-
-	want := "I"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	tests := []struct {
+		name, want string
+		arabic     int
+	}{
+		{name: "1 gets converted to I", arabic: 1, want: "I"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := ConvertToRoman(tt.arabic)
+			if got != tt.want {
+				t.Errorf("ConvertToRoman(%d) = %q, want %q", tt.arabic, got, tt.want)
+			}
+		})
 	}
 }
