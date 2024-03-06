154dd78eaa46442ec78961da9282b62fe5bf4566
Cleberson <pauluci.cleberson@gmail.com>
2024-03-05 21:29:26 -0300

    Created v5 for testing arabic convertion to roman numbers

---
+++ go-fundamentals/property/v5/numerals.go
@@ -0,0 +1,19 @@
+package v5
+
+import "strings"
+
+// ConvertToRoman converts an integer to a roman numeral
+func ConvertToRoman(arabic int) string {
+
+	var result strings.Builder
+
+	for i := 0; i < arabic; i++ {
+		if arabic == 4 {
+			result.WriteString("IV")
+			break
+		}
+		result.WriteString("I")
+	}
+
+	return result.String()
+}
---
+++ go-fundamentals/property/v5/numerals_test.go
@@ -0,0 +1,23 @@
+package v5
+
+import "testing"
+
+func TestConvertToRoman(t *testing.T) {
+	cases := []struct {
+		Description, Want string
+		Arabic            int
+	}{
+		{Description: "1 gets converted to I", Arabic: 1, Want: "I"},
+		{Description: "2 gets converted to II", Arabic: 2, Want: "II"},
+		{Description: "3 gets converted to III", Arabic: 3, Want: "III"},
+		{Description: "4 gets converted to IV", Arabic: 4, Want: "IV"},
+	}
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			got := ConvertToRoman(test.Arabic)
+			if got != test.Want {
+				t.Errorf("ConvertToRoman(%d) = %q, want %q", test.Arabic, got, test.Want)
+			}
+		})
+	}
+}
