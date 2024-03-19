3c36283ada09fb4dedfa5efecfe5c03cc1b8e850
Cleberson <pauluci.cleberson@gmail.com>
2024-03-19 18:46:30 -0300

    First created test for secondsInRadinas

---
+++ go-fundamentals/maths/v2/clockface_test.go
@@ -0,0 +1,17 @@
+package v2
+
+import (
+	"math"
+	"testing"
+	"time"
+)
+
+func TestSecondsInRadians(t *testing.T) {
+	thirtySeconds := time.Date(312, time.October, 28, 0, 0, 30, 0, time.UTC)
+	want := math.Pi
+	got := secondsInRadians(thirtySeconds)
+
+	if want != got {
+		t.Errorf("wanted %v radians, but got %v", want, got)
+	}
+}
