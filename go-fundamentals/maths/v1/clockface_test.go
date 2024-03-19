43d4c403795be5ed7b81e5c587783116ddbe5d30
Cleberson <pauluci.cleberson@gmail.com>
2024-03-19 18:38:33 -0300

    Writed acceptance test for secondHandAt30Seconds

--- go-fundamentals/maths/v1/clockface_test.go
+++ go-fundamentals/maths/v1/clockface_test.go
@@ -14,4 +14,16 @@ func TestSecondHandAtMidnight(t *testing.T) {
 	if got != want {
 		t.Errorf("got %v, wanted %v", got, want)
 	}
+
+}
+
+func TestSecondHandAt30Seconds(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+
+	want := Point{X: 150, Y: 150 + 90}
+	got := SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
 }
